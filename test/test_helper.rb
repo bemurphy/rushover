53dabbbb5928059587d86929501fc65bf0228262
Brendon Murphy <xternal1+github@gmail.com>
2012-08-29 22:12:48 -0700

    Line spacing in test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,7 +7,9 @@ FakeWeb.allow_net_connect = false
 
 class RushoverTest < Test::Unit::TestCase
   setup do
-    FakeWeb.register_uri(:post, "https://api.pushover.net/1/messages.json", :body => { :status => 1 }.to_json, :content_type => "application/json")
+    FakeWeb.register_uri(:post, "https://api.pushover.net/1/messages.json",
+                         :body => { :status => 1 }.to_json,
+                         :content_type => "application/json")
   end
 
   teardown do
